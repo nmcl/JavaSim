0e7b91d4859c06a92672419d0e7748cc44cc1796
Mark Little <mlittle@redhat.com>
2013-05-31 13:37:01 +0100

    Added more unit tests.

---
+++ src/test/java/org/javasim/tests/streams/ErlangStreamUnitTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 1990-2008, Mark Little, University of Newcastle upon Tyne
+ * and others contributors as indicated 
+ * by the @authors tag. All rights reserved. 
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors. 
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA  02110-1301, USA.
+ * 
+ * (C) 1990-2008,
+ */
+
+package org.javasim.tests.streams;
+
+import org.javasim.streams.ErlangStream;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ErlangStreamUnitTest
+{
+    @Test
+    public void test () throws Exception
+    {
+        ErlangStream str = new ErlangStream(10.0, 4.0);
+        
+        try
+        {
+            str.getNumber();
+        }
+        catch (final Exception ex)
+        {
+            fail();
+        }
+        
+        str = new ErlangStream(10.0, 1.0, 1000);
+        
+        try
+        {
+            str.getNumber();
+        }
+        catch (final Exception ex)
+        {
+            fail();
+        }
+        
+        str = new ErlangStream(20.0, 2.0, 1000, 772532, 1878892441);
+        
+        try
+        {
+            str.getNumber();
+        }
+        catch (final Exception ex)
+        {
+            fail();
+        }
+    }
+}
---
+++ src/test/java/org/javasim/tests/streams/UniformStreamUnitTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 1990-2008, Mark Little, University of Newcastle upon Tyne
+ * and others contributors as indicated 
+ * by the @authors tag. All rights reserved. 
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors. 
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA  02110-1301, USA.
+ * 
+ * (C) 1990-2008,
+ */
+
+package org.javasim.tests.streams;
+
+import org.javasim.streams.UniformStream;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UniformStreamUnitTest
+{
+    @Test
+    public void test () throws Exception
+    {
+        UniformStream str = new UniformStream(10.0, 2.0);
+        
+        try
+        {
+            str.getNumber();
+            str.error();
+        }
+        catch (final Exception ex)
+        {
+            fail();
+        }
+        
+        str = new UniformStream(10.0, 1.0, 1000);
+        
+        try
+        {
+            str.getNumber();
+        }
+        catch (final Exception ex)
+        {
+            fail();
+        }
+        
+        str = new UniformStream(20.0, 5.0, 1000, 772532, 1878892441);
+        
+        try
+        {
+            str.getNumber();
+            str.error();
+        }
+        catch (final Exception ex)
+        {
+            fail();
+        }
+    }
+}
